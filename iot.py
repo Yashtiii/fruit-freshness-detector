30923092ec4ee4d8af4c3ba9a3e1b301eaba93f8
Vishal-Bytee <Sharmavishal6163@gmail.com>
2025-09-02 03:24:37 +0530

    Update iot.py

--- iot.py
+++ iot.py
@@ -1,12 +1,12 @@
 import serial
 import time
 
-# same port as Arduino IDE me show ho raha hai (abhi tumne bola COM5 hai)
+
 arduino = serial.Serial('COM5', 115200, timeout=1)
-time.sleep(2)  # connection stabilize
+time.sleep(2)  
 
 while True:
-    if arduino.in_waiting > 0:   # check if data is available
+    if arduino.in_waiting > 0:  
         line = arduino.readline().decode('utf-8', errors='ignore').strip()
         if line:
             print("From ESP32:", line)
